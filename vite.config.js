91bb7e2976ca53d8faa6cf4ac6cd0f956e8c3986
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-07-29 12:14:35 +0200

    modifciando enrutamiento

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    cors:true,
+    cors: true,
     mimeTypes: {
       'application/javascript': ['mjs'],
+    },
+    historyApiFallback: true, // Esto maneja las rutas en modo desarrollo
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html', // Asegúrate de que esto apunte a tu archivo index.html
     },
   },
 });

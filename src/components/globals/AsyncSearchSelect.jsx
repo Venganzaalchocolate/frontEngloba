385f33ad52427f6a591cdf30655662659b82c413
venganzaalchocolate <venganzaalchocolate@gmail.com>
2025-10-31 10:49:08 +0100

    gestion de programas y dispositivos nuevo import

--- src/components/globals/AsyncSearchSelect.jsx
+++ src/components/globals/AsyncSearchSelect.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "../styles/ModalForm.module.css";
+import styles from "../styles/modalForm.module.css";
 import useDebounce from "../../lib/utils";
 
 const AsyncSearchSelect = ({ field, onChange }) => {

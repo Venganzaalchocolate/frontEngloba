c740633ad3123bdd2df3c30a3352ce536646bfbb
venganzaalchocolate <venganzaalchocolate@gmail.com>
2026-03-18 09:08:41 +0100

    fix: update fast-xml-parser override + auto reload on chunk error

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { LoggedProvider } from './context/LoggedProvider.jsx'
-import React, { createContext, useContext } from 'react';
+import React from 'react';
+
+window.addEventListener('error', (event) => {
+  const msg = event?.message || '';
+
+  if (
+    msg.includes('Failed to fetch dynamically imported module') ||
+    msg.includes('Importing a module script failed')
+  ) {
+    window.location.reload();
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <LoggedProvider>
-      <App />
-    </LoggedProvider>
+  <LoggedProvider>
+    <App />
+  </LoggedProvider>
 )
